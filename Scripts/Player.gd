4c609df34309a4dcf46ce3f8bb3f24249a252881
Travis A. Carlen <tcarlen@ucsc.edu>
2023-05-22 00:22:57 -0700

    Corrected file storage, added parallax, added player sprites and reticle

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,17 +17,21 @@ func _ready():
 func _physics_process(delta):
 	var motion=Vector2()
 	
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up") and position.y > -250:
+		$AnimationPlayer.play("Up")
 		motion.y-=1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down") and position.y < 270:
+		$AnimationPlayer.play("Down")
 		motion.y+=1
 	if Input.is_action_pressed("move_left"):
+		$AnimationPlayer.play("Left")
 		motion.x-=1
 	if Input.is_action_pressed("move_right"):
+		$AnimationPlayer.play("Right")
 		motion.x+=1
 	motion=motion.normalized()
 	motion=move_and_slide(motion*movespeed)
-	look_at(get_global_mouse_position())
+	$Gun.look_at(get_global_mouse_position())
 	
 	if(Input.is_action_just_pressed("action_fire")):
 		fire()
@@ -35,9 +39,9 @@ func _physics_process(delta):
 	
 func fire():
 	var bullet_in=bullet.instance()
-	bullet_in.position = $Gun.get_global_position()
-	bullet_in.rotation_degrees= rotation_degrees
-	bullet_in.apply_impulse(Vector2(),Vector2(bulletspeed,0).rotated(rotation))
+	bullet_in.position = $Gun/Reticle.get_global_position()
+	bullet_in.rotation_degrees= $Gun.rotation_degrees
+	bullet_in.apply_impulse(Vector2(),Vector2(bulletspeed,0).rotated($Gun.rotation))
 	get_tree().get_root().call_deferred("add_child",bullet_in)
 
 func _on_hurtbox_area_entered(area):
