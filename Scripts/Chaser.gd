0671865ea6a3e90d580443ff2277947156d8bb15
Travis A. Carlen <tcarlen@ucsc.edu>
2023-05-26 19:30:48 -0700

    Health Bars implemented, enemy modularity increased

--- Scripts/Chaser.gd
+++ Scripts/Chaser.gd
@@ -5,13 +5,17 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var velocity = Vector2.ZERO
-var speed = 20
-var awake = false
+export var hp = 10
+export var hpMax = 10
+export var speed = 20
+export var awake = false
 
 signal enemy_defeated
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if awake:
+		$Sprite.modulate = Color.white
+		$WakeBox.queue_free()
 
 
 func _physics_process(delta):
@@ -23,11 +27,16 @@ func _physics_process(delta):
 
 func _on_HurtBox_area_entered(area):
 	if area.is_in_group("Bullet") and awake:
+		hp -= 1
+		updateHP()
+	if hp == 0:
 		emit_signal("enemy_defeated")
 		queue_free()
 
+func updateHP():
+	$ColorRect2/ColorRect.rect_size.x = lerp(0,40,(float(hp)/hpMax))
 
 func _on_WakeBox_area_entered(area):
 	if area.is_in_group("Player"):
-		$MummyEnemy1.modulate = Color.white
+		$Sprite.modulate = Color.white
 		awake = true
