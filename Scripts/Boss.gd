2adb93b257d2001591cc976959c920015d38b88e
Justin Park <jutpark@ucsc.edu>
2023-06-03 22:17:02 -0700

    Boss temp kinda not working copium will fix soon

--- Scripts/Boss.gd
+++ Scripts/Boss.gd
@@ -8,14 +8,14 @@ var hp = 10
 var hpMax = 10
 var speed = 20
 var movespeed=200
-var bulletspeed=500
+var bulletspeed=200
 var bullet=preload("res://Assets/Prefabs/Enemies/Enemy Bullet.tscn")
 export var awake = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if awake:
 		$Sprite.modulate = Color.white
-	fire_arrow() # Replace with function body.
+	fire_arrow(1) # Replace with function body.
 
 func _on_HurtBox_area_entered(area):
 	print(awake)
@@ -33,12 +33,33 @@ func _on_HurtBox_area_entered(area):
 func updateHP():
 	$Node2D/ColorRect2/ColorRect.rect_size.x = lerp(0,40,(float(hp)/hpMax))
 
-func fire_arrow():
+func fire_bullet(direction,x_offset,y_offset,angle):
+	#1=right
+	#2=up
+	#3=left
+	#4=down
 	var bullet_in=bullet.instance()
-	bullet_in.position = $Gun/Sprite.get_global_position()
-	bullet_in.rotation_degrees= $Gun.rotation
-	bullet_in.apply_impulse(Vector2(),Vector2(bulletspeed,0))
+	if(direction==1):
+		bullet_in.position = Vector2($right/Sprite.get_global_position().x+x_offset, $right/Sprite.get_global_position().y+y_offset)
+		bullet_in.rotation_degrees= $right.rotation
+	if(direction==2):
+		bullet_in.position = Vector2($up/Sprite.get_global_position().x+x_offset, $up/Sprite.get_global_position().y+y_offset)
+		bullet_in.rotation_degrees= $up.rotation
+	if(direction==3):
+		bullet_in.position = Vector2($left/Sprite.get_global_position().x+x_offset, $left/Sprite.get_global_position().y+y_offset)
+		bullet_in.rotation_degrees= $left.rotation
+	if(direction==4):
+		bullet_in.position = Vector2($down/Sprite.get_global_position().x+x_offset, $down/Sprite.get_global_position().y+y_offset)
+		bullet_in.rotation_degrees= $down.rotation
+	#bullet_in.position.x+offset;
+	#bullet_in.position.y+offset;
+	
+	bullet_in.apply_impulse(Vector2(),Vector2(bulletspeed,angle))
 	get_tree().get_root().call_deferred("add_child",bullet_in)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func fire_arrow(direction):
+	fire_bullet(direction,0,10,480);
+	fire_bullet(direction,0,-10,-180);
+	fire_bullet(direction,0,0,0);
